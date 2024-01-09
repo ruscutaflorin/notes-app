f611e522e4508b966ce186773bab1d6cd4b55e27
Florin Ruscuta <ruscutaf@gmail.com>
2024-01-10 01:49:16 +0200

    change the app port

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
