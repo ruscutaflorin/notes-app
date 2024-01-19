5996366fdf002890682b61944d2ab327fb33e1e8
tudorrusu3 <tudorrusu133@yahoo.com>
2024-01-19 16:20:25 +0200

    notes page css

---
+++ client/src/components/user/UserDetails.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+const UserDetails = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const { user } = useAuthContext();
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        console.log(user);
+        if (user) {
+          const response = await axios.get(
+            `http://localhost:3001/api/notes/get-details?email=${user.username}`
+          );
+          setUserInfo(response.data);
+          console.log("here", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching user info:", error.message);
+      }
+    };
+
+    fetchUserInfo();
+  }, [user]);
+
+  return (
+    <div className="user-details">
+      {userInfo ? (
+        <>
+          <p>Username: {userInfo.username}</p>
+          <p>Email: {userInfo.email}</p>
+          <p>ID: {userInfo.id}</p>
+        </>
+      ) : (
+        <p>Loading user information...</p>
+      )}
+    </div>
+  );
+};
+
+export default UserDetails;
